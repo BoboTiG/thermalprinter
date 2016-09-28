4c825c61d6b64eaf02c9483a370121ea26955ee4
Tiger-222 <contact@tiger-222.fr>
2016-09-28 11:07:00 +0200

    tests: create FakeThermalPrinter class to erase timers

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,16 @@ import pytest
 from thermalprinter import ThermalPrinter
 
 
+class FakeThermalPrinter(ThermalPrinter):
+    def __init__(self, port):
+        super().__init__(port=port)
+
+        # Disable timers
+        self._byte_time = 0
+        self._dot_feed_time = 0
+        self._dot_print_time = 0
+
+
 def pytest_addoption(parser):
     txt = 'Serial port to use (examples: /dev/ttyAMA0, /dev/ttyS0 [default])'
     parser.addoption('--port', action='store', default='/dev/ttyS0', help=txt)
@@ -20,7 +30,7 @@ def port(request):
 
 @pytest.fixture(scope='module')
 def printer(port):
-    return ThermalPrinter(port=port)
+    return FakeThermalPrinter(port=port)
 
 
 @pytest.fixture(scope='session')
