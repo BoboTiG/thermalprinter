b9ab50b91b4509e8132dc524415c11254f9cc79f
Tiger-222 <contact@tiger-222.fr>
2016-10-01 13:05:07 +0200

    Fix semver bad count (from 0.0.2 to 0.10.0, seriously?!)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ author = 'Tiger-222'
 # built documents.
 #
 # The short X.Y version.
-version = '0.10.4'
+version = '0.1.4'
 # The full version, including alpha/beta/rc tags.
 release = 'latest'
 
