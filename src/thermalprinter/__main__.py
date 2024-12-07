e897b282a52cea663fa376dac9dc063cd6ec4396
Mickaël Schoentgen <contact@tiger-222.fr>
2024-12-07 10:48:16 +0100

    fix

--- src/thermalprinter/__main__.py
+++ src/thermalprinter/__main__.py
@@ -1,6 +1,14 @@
-if __name__ == "__main__":
+def main() -> int:
     from thermalprinter import ThermalPrinter
     from thermalprinter.tools import calibrate
 
     with ThermalPrinter(run_setup_cmd=False) as printer:
         calibrate(printer)
+
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(main())
